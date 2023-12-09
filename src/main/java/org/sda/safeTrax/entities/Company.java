0a1c3e55f4c2dcdff3194937599f9e5dcd9e400a
arismamo <arismamo@gmail.com>
2023-12-09 11:39:42 +0100

    - Added hibernate configuration - Added entities

--- src/main/java/org/sda/safeTrax/entities/Company.java
+++ src/main/java/org/sda/safeTrax/entities/Company.java
@@ -1,9 +1,8 @@
 package org.sda.safeTrax.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "st_company")
@@ -13,4 +12,7 @@ public class Company extends BaseEntity{
 
     @Column(name = "phone")
     private String phone;
+
+    @OneToMany
+    List<Order> orders;
 }
--- src/main/java/org/sda/safeTrax/entities/Order.java
+++ src/main/java/org/sda/safeTrax/entities/Order.java
@@ -16,4 +16,32 @@ public class Order extends BaseEntity {
 
     @Column(name = "price")
     private Float price;
+
+    @Column(name = "weight")
+    private Float weight;
+
+    @Column(name = "dimension")
+    private String dimension;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "client_first_name")
+    private String clientFirstName;
+
+    @Column(name = "client_last_name")
+    private String clientLastName;
+
+    @Column(name = "client_address")
+    private String clientAddress;
+
+    @Column(name = "client_phone")
+    private String clientPhone;
+
+    @Column(name = "company_id")
+    private Long companyId;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
+    private Company company;
 }
