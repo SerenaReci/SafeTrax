6500a5193bb2d102f2b5e4cd12a544d1b68363db
arismamo <arismamo@gmail.com>
2023-12-09 15:48:35 +0100

    - Added menu selection logic to main class

--- src/main/java/org/sda/safeTrax/Main.java
+++ src/main/java/org/sda/safeTrax/Main.java
@@ -17,15 +17,32 @@ public class Main {
         System.out.println(" Please select an action:");
 
         System.out.println();
-        String selection = "";
-        while (!selection.equals("exit")) {
+        String selection = null;
+
+        while (!"exit".equals(selection)) {
             System.out.println("(1) - Type 1 to insert a package");
             System.out.println("(2) - Type 2 to change package status");
             System.out.println("(3) - Type 3 to show details");
             System.out.println("(4) - Type 'exit' to close the application");
-
-
             selection = scanner.nextLine();
+
+            if ("1".equals(selection)) {
+                // Read package details
+
+            }
+            else if ("2".equals(selection)) {
+                // Show package detail
+
+            }
+            else if ("3".equals(selection)) {
+                // Do change status logic
+
+            } else if ("exit".equals(selection)) {
+                // Do nothing.
+            }
+            else {
+                System.out.println("----- Not a valid selection! -----");
+            }
         }
     }
 }
