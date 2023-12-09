4c62cbaefe44d6ae9207cce2868c2be5c367d507
arismamo <arismamo@gmail.com>
2023-12-09 15:39:38 +0100

    - Added menu selection logic to main class

--- src/main/java/org/sda/safeTrax/Main.java
+++ src/main/java/org/sda/safeTrax/Main.java
@@ -11,5 +11,21 @@ public class Main {
         Scanner scanner=new Scanner(System.in);
         SessionFactory factory = HibernateConfiguration.getSessionFactory();
 
+        System.out.println();
+        System.out.println();
+        System.out.println("----------- Welcome to SafeTrax -------------");
+        System.out.println(" Please select an action:");
+
+        System.out.println();
+        String selection = "";
+        while (!selection.equals("exit")) {
+            System.out.println("(1) - Type 1 to insert a package");
+            System.out.println("(2) - Type 2 to change package status");
+            System.out.println("(3) - Type 3 to show details");
+            System.out.println("(4) - Type 'exit' to close the application");
+
+
+            selection = scanner.nextLine();
+        }
     }
 }
