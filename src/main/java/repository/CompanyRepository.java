c7dca096932f0ebeca23c804cdc5c62907de1833
User <serenareci1@gmail.com>
2023-12-09 15:04:50 +0100

    Created Entitees

---
+++ src/main/java/repository/CompanyRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+public class CompanyRepository {
+    private  CompanyRepository companyRepository;
+    private String title;
+    private String phone;
+
+    public CompanyRepository(String title,String phone){
+        this.title=title;
+        this.phone=phone;
+
+    }
+}
